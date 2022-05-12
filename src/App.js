cf04d701f7139e14574c8dce5e2f51b871f43e24
Muhammad Khairul Rizal Purwanto <rizal.purwanto.id@gmail.com>
2022-05-12 07:02:07 +0700

    added routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from './component/Navbar';
+import Home from './views/Home';
+import AddPokemon from './views/AddPokemon';
+import Type from './views/Type';
+import Details from './views/Details';
+
 function App() {
   return (
     <div className="App">
